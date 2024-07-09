81c36e366adca0728f31460cc0276075cb3621bd
alexsparkes <turbomarshmello@gmail.com>
2024-07-10 00:48:12 +0100

    fix: Improved projects view on mobile devices

--- components/sections/work.tsx
+++ components/sections/work.tsx
@@ -1,12 +1,13 @@
 import { Card } from "@/components/ui/card";
-import projects from '@/data/projects.json';
+import projects from "@/data/projects.json";
 
 export const Work = () => {
-
   return (
     <section id="work">
-      <div className="mx-auto w-2/3 py-10">
-        <h1 className="text-[50px] md:text-[75px] xl:text-[100px]">My Work</h1>
+      <div className="mx-auto lg:w-2/3 w-full py-10">
+        <h1 className="text-[50px] md:text-[75px] xl:text-[100px] mx-5">
+          My Work
+        </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-10 ">
           {projects.map((card, index) => (
             <Card
