1821aeab7abe67d56292f116e1e554448edde3c4
alexsparkes <alexsparkes@gmail.com>
2024-07-10 17:18:56 +0100

    feat: add gallery

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
     <>
       <Hero />
       <About />
-      {/*<Gallery />*/}
+      <Gallery />
       <Work />
     </>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'cdn.muetab.com',
+            port: '',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
